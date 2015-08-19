8e509f8c8cd22b0aa13c2f12b6cdd48a15cf64ec
Andrew Homeyer <homeyer@gmail.com>
2015-08-19 09:53:26 -0600

    support ssl

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,15 @@
 
 docker rm -f nginx && \
 docker build --tag "waffleio/takeout-nginx" . && \
-docker run -d --name nginx --link app --link api --link hooks --link poxa -p 80:80 waffleio/takeout-nginx && \
+docker run -d \
+  --name nginx \
+  --link app \
+  --link api \
+  --link hooks \
+  --link poxa \
+  -p 443:443 \
+  -v /etc/waffle/nginx/certs:/etc/nginx/certs:ro \
+  waffleio/takeout-nginx && \
 sleep 2 && \
 docker ps && \
 # docker exec -it nginx cat /etc/nginx/nginx.conf && \
