1826094ab2cb44547a2c51e29bec904ed24c2a0c
Andrew Homeyer <homeyer@gmail.com>
2015-09-23 18:05:58 -0600

    adding readme, fixing helper scripts

---
+++ README.md
@@ -0,0 +1,7 @@
+takeout-nginx
+=============
+
+This is the nginx container used in Waffle Takeout.
+
+### For development
+The [rebuild.sh](rebuild.sh) and [restart-all.sh](restart-all.sh) commands are only used in dev, just for the convenience of rapidly rebuilding the docker image and restarting other dependent images.
--- restart-all.sh
+++ restart-all.sh
@@ -1,48 +1,48 @@
-docker rm -f app
-docker rm -f api
-docker rm -f hooks
-docker rm -f hedwig
-docker rm -f rally
-docker rm -f poxa
+docker rm -f waffle-app
+docker rm -f waffle-api
+docker rm -f waffle-hooks
+docker rm -f waffle-hedwig
+docker rm -f waffle-rally-integration
+docker rm -f waffle-poxa
 
 docker run \
   -d \
-  --name app \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-app \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
   quay.io/waffleio/waffle.io-app
 
 docker run \
   -d \
-  --name api \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-api \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
    quay.io/waffleio/waffle.io-api
 
 docker run \
   -d \
-  --name hooks \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-hooks \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
   quay.io/waffleio/waffle.io-hooks
 
 docker run \
   -d \
-  --name hedwig \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-hedwig \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
   quay.io/waffleio/hedwig
 
 docker run \
   -d \
-  --name rally \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-rally-integration \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
   quay.io/waffleio/waffle.io-rally-integration
 
 docker run \
   -d \
-  --name poxa \
-  --env-file /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/environment.list \
-  -v /Users/ahomeyer/Desktop/waffle-takeout/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
+  --name waffle-poxa \
+  --env-file /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/environment.list \
+  -v /Users/ahomeyer/Desktop/waffleio-takeout/etc/waffle/ca-certificates:/etc/waffle/ca-certificates:ro \
   quay.io/waffleio/poxa
